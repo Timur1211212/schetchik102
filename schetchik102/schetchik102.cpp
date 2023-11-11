92c78b1568836172b63c34bcdcf1b5c6e00f87df
Timur Magomedov <mew678hh@gmail.com>
2023-11-11 15:16:03 +0300

    Добавьте файлы проекта.

---
+++ schetchik102/schetchik102.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <stdexcept>
+#include <string>
+
+using namespace std;
+
+class Counter
+{
+public:
+
+    Counter(int izmenenie) { this->izmenenie = izmenenie; }
+
+    void process_plus()
+    {
+        izmenenie++;
+    }
+
+    void process_minus()
+    {
+        izmenenie--;
+    }
+
+    int get_pokazanie()
+    {
+        return izmenenie;
+    }
+
+private:
+    int izmenenie;
+};
+
+int main()
+{
+    setlocale(LC_CTYPE, "Russian");
+    int n = 0;
+    string soglasie;
+    do {
+        cout << "Вы хотите указать начальное значение счётчика ? Введите да или нет: ";
+        cin >> soglasie;
+        if (soglasie == "yes") { cout << "Введите начальное значение счётчика: "; }
+        else if (soglasie == "no") { soglasie = n; }
+        else { cout << "Неправильный ввод" << endl; }
+    } while (soglasie != "yes" && soglasie != "no");
+    cin >> n;
+    auto schet = Counter(n);
+    while (1)
+    {
+        char g;
+        cout << "Введите команду('+', '-', '=' или 'x') :";
+        cin >> g;
+        if (g != 'x')
+        {
+            if (g == '+')
+            {
+                schet.process_plus();
+            }
+            else if (g == '-')
+            {
+                schet.process_minus();
+            }
+            else if (g == '=')
+            {
+                cout << schet.get_pokazanie() << endl;
+            }
+            else
+            {
+                cout << "Неправильный символ!" << endl;
+            }
+        }
+        else
+        {
+            cout << "До свидания!";
+            break;
+        }
+    }
+    return 0;
+}
